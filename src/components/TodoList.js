e23a0e0b99403a929c3114599ccfdbf163daf4e3
Sian Burgess <sian_burgess@hotmail.co.uk>
2020-09-30 21:43:06 +0100

    Implemented basic HTML & CSS and created components

---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TodoList() {
+  return (
+    <div className="TodoList">
+      <div className="todo-container">
+        <ul className="todo-list"></ul>
+      </div>
+    </div>
+  );
+}
